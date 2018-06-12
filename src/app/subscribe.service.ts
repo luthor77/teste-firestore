02941d60f7712c82391c2f3a6d01cb6ec78b4a38
luthor77 <vithor.af@gmail.com>
2018-06-12 16:45:14 -0300

    Quase perfeito

--- src/app/subscribe.service.ts
+++ src/app/subscribe.service.ts
@@ -5,131 +5,112 @@ import { Observable } from 'rxjs/Rx';
 @Injectable()
 export class SubscribeService {
 	array = [];
-	merge: any;
 
 	constructor(private db: AngularFirestore) {}
 
-	// cria o observable do tipo de atendimento
+	// cria o observable referente ao caminho especificado
 	tipoAtendimento(path) {
 		return this.db.doc(path).snapshotChanges()
 	}
 
+	// retorna um array asincrono que monitora as mudanças na tabela
 	retornaArrayObservable() {
 		return this.db.collection('testeFire').stateChanges()
-			.flatMap(tfCollection => {
-				let tipoAtendimento
-
+			.switchMap(tfCollection => {
 				tfCollection.forEach(doc => {
-					switch (doc.payload.type) {
-						case 'added':
-							{
-								// cria um observable unindo os observables de cada tipo de atendimento
-								if (doc.payload.doc.data()['atendimento']) {
-									tipoAtendimento = this.reDoMerge()
-									this.pushArray(doc)
-								} else {
-									this.array.push({
-										key: doc.payload.doc.id,
-										teste: doc.payload.doc.data()['teste']
-									})
-								}
-
-								break;
-							}
-						case 'modified':
-							{
-								tipoAtendimento = this.modificaArray(doc)
-								break;
-							}
-					}
+					// se for uma adição, coloca no array
+					doc.payload.type === 'added' ? this.pushArray(doc) :
+					// se for uma alteração, modifica o array
+					doc.payload.type === 'modified' ? this.modificaArray(doc) :
+					// se for uma remoção, remove o objeto referente a ele no array
+					doc.payload.type === 'removed' ? this.array = this.array.filter(obj => obj['key'] != doc.payload.doc.id) :
+					null
 				})
 
-				return tipoAtendimento
+				// retorna observables dos tipos de atendimento referidos nos documentos da coleção testeFire
+				return this.createAtendimentoObs()
 			})
 			.map(tipo_atendimento => {
-				console.log(tipo_atendimento['payload'].data()['tipo']);
+				console.log(tipo_atendimento.payload.data());
 
+				// verifica a posição do array e o id para alterar o dado somente na posição específica quando há modificações
+				// no tipo de atendimento
 				return this.verificaArray(tipo_atendimento)
 			})
 			// impede que o async pipe crie vários subscribes se for utilizado mais de uma vez no template
 			.share()
-
 	}
 
-	reDoMerge() {
+
+	createAtendimentoObs() {
 		return this.db.collection('testeFire').valueChanges().take(1).switchMap(tfCollection => {
-			let tipoAtendimento = [];
+			let tipo_atendimentos = [];
 
 			tfCollection.forEach(doc => {
-				if (doc['atendimento']) {
-					tipoAtendimento.push(doc['atendimento'].path)
-				}
+				doc['atendimento'] ? tipo_atendimentos.push(doc['atendimento'].path) : null
 			})
 
-			let uniqueArray = tipoAtendimento.filter(function(item, pos, self) {
-				return self.indexOf(item) === pos;
+			// tira as referências duplicadas do array
+			tipo_atendimentos = Array.from(new Set(tipo_atendimentos))
+
+			// transforma as referências em observables
+			tipo_atendimentos.forEach((element, index, array) => {
+				array[index] = this.tipoAtendimento(element)
 			})
 
-			tipoAtendimento = []
-
-			uniqueArray.forEach(element => {
-				tipoAtendimento.push(this.tipoAtendimento(element))
-			})
-
-			return Observable.merge(...tipoAtendimento)
+			// faz um merge entre os observables
+			return Observable.merge(...tipo_atendimentos)
 		})
 	}
 
-	// verifica a posição do array e o id para alterar o dado somente na posição específica
 	verificaArray(tipo) {
 		this.array.forEach(obj => {
-			if (obj['id_atendimento'] === tipo['payload'].id) {
-				obj['tipo'] = tipo['payload'].data()['tipo'];
-			}
+			if (obj['id_atendimento'] === tipo['payload'].id)
+				if (obj['tipo'] != tipo['payload'].data()['tipo'])
+					obj['tipo'] = tipo['payload'].data()['tipo'];
 		})
 		return this.array
 	}
 
 	modificaArray(testeFire) {
-		for (const obj in this.array) {
-			if (this.array.hasOwnProperty(obj)) {
-				// verifica se há um objeto no array referente ao documento
-				if (this.array[obj]['key'] === testeFire.payload.doc.id) {
-					// verifica se há a propriedade atendimento no documento
-					if (testeFire.payload.doc.data()['atendimento']) {
-						let id = testeFire.payload.doc.data()['atendimento'].path.split('/', 2);
-						//verifica se a propriedade atendimento possui o mesmo id do objeto no array e adiciona
-						if (id[1] != this.array[obj]['id_atendimento']) {
-							this.array[obj]['id_atendimento'] = id[1]
-						}
-						// se a propriedade atendimento não existir no documento
-					} else {
-						// verifica se a propriedade existe no objeto do array referente ao documento e exclui as propriedades relacionas
-						if (this.array[obj]['id_atendimento']) {
-							delete this.array[obj]['id_atendimento']
-							delete this.array[obj]['tipo']
-						}
-					}
+		this.array.forEach(element => {
+			if (element['key'] === testeFire.payload.doc.id) {
+				// verifica se há a propriedade atendimento no documento
+				if (testeFire.payload.doc.data()['atendimento']) {
+					let id = testeFire.payload.doc.data()['atendimento'].path.split('/', 2)[1];
+					//verifica se a propriedade atendimento possui o mesmo id do objeto no array e adiciona
+					if (id != element['id_atendimento'])
+						element['id_atendimento'] = id
 
-					if (this.array[obj]['teste'] != testeFire.payload.doc.data()['teste']) {
-						this.array[obj]['teste'] = testeFire.payload.doc.data()['teste']
+				} else {
+					// se a propriedade atendimento não existir no documento
+					// verifica se a propriedade existe no objeto do array referente ao documento e exclui as propriedades relaciona
+					if (element['id_atendimento']) {
+						element['id_atendimento'] = ''
+						element['tipo'] = ''
 					}
 				}
-			}
-		}
 
-		return this.reDoMerge()
+				if (element['teste'] != testeFire.payload.doc.data()['teste'])
+					element['teste'] = testeFire.payload.doc.data()['teste']
+			}
+		})
 	}
 
-	// preenche o array
 	pushArray(testeFire) {
+		// verifica se existe a propriedade atendimento no documento
+		(testeFire.payload.doc.data()['atendimento']) ?
 		this.tipoAtendimento(testeFire.payload.doc.data()['atendimento'].path).take(1).subscribe(doc => {
+				this.array.push({
+					key: testeFire.payload.doc.id,
+					id_atendimento: doc.payload.id,
+					tipo: doc.payload.data()['tipo'],
+					teste: testeFire.payload.doc.data()['teste']
+				})
+			}):
 			this.array.push({
 				key: testeFire.payload.doc.id,
-				id_atendimento: doc.payload.id,
-				tipo: doc.payload.data()['tipo'],
 				teste: testeFire.payload.doc.data()['teste']
 			})
-		})
 	}
 }
